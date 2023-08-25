8b2be9b6a2b41d86824f049a33605a219365b075
dheeraj755148 <dheerajdayakaran95@gmail.com>
2023-08-25 12:04:19 +0530

    env changes

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-const bodyParser = require("body-parser");
-const routes = require("./routes");
-const morgan = require("morgan");
+require('dotenv').config() // Load environment variables from .env file
 
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const morgan = require('morgan')
+
+const mongoose = require('mongoose')
 
 app.use(express.json())
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan("dev"));
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'))
 
-const port = 9000;
+const port = 9000
 
-
-const connec_url = "mongodb+srv://admin:84pN1ItLCw8hTecU@cluster0.1aci9.mongodb.net/bloodDonation?retryWrites=true&w=majority"
-mongoose.connect(connec_url, {}, (err) =>{
-    if (err) {
-        console.log(err.message);
-        throw err;
-      }
-      console.log(`mongodb server started...`);
+mongoose.connect(process.env.DB_URL_NET, {}, (err) => {
+  if (err) {
+    console.log(err.message)
+    throw err
+  }
+  console.log(`mongodb server started...`)
 })
 
-app.use(routes);
+app.use(routes)
 
-app.listen(port, ()=>console.log(`Listening at: ${port}`))
+app.listen(port, () => console.log(`Listening at: ${port}`))
 
-module.exports = app;
-
+module.exports = app
